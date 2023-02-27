7b850d787cc0fd0cff8323edea5219d67cb70f88
martinvalenciap <125387154+martinvalencia001@users.noreply.github.com>
2023-02-27 22:59:28 +0000

    added the stable and horse class to keep an array of the information

--- Main.java
+++ Main.java
@@ -1,33 +1,56 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    //Ask the user here
-	  Stable s = new Stable();
-		System.out.println("What is the address of the stable?");
-		Scanner scanner = new Scanner(System.in);
-		s.address = scanner.nextLine();
-		do {
-			System.out.println("Do you wish to add a horse to the stable?"); 
-			char input = scanner.next().charAt(0);
-			scanner.nextLine();
-			if(input == 'y'){
-				Horse h;
-				System.out.println("What is the name of the horse?");
-				String hn = scanner.nextLine();
-				System.out.println("What is the weight of the horse?");
-				double w = scanner.nextDouble();
-				System.out.println("Enter true if the horse is tame or false if it is not.");
-				boolean t = scanner.nextBoolean();
-				h = new Horse(hn, w, t);
-				s.arrayList.add(h);
-			}//testing goodest 4
-			else{
-				break;
-			}
-		}while(true);
-		//Display the info here
-                //You code here!
-	  }
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the address of the stable? ");
+        String address = input.nextLine();
+        Stable stable = new Stable(address);
+
+        while (true) {
+            System.out.print("Do you wish to add a horse to the stable? (y/n) ");
+            String choice = input.nextLine();
+            if (choice.equals("n")) {
+                break;
+            } else if (!choice.equals("y")) {
+                System.out.println("Invalid input. Please enter 'y' or 'n'.");
+                continue;
+            }
+
+            System.out.print("What is the name of the horse? ");
+            String name = input.nextLine();
+
+            double weight;
+            while (true) {
+                System.out.print("What is the weight of the horse? ");
+                try {
+                    weight = Double.parseDouble(input.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a number.");
+                }
+            }
+
+            boolean isTame;
+            while (true) {
+                System.out.print("Enter true if the horse is tame or false if it is not: ");
+                String tameInput = input.nextLine();
+                if (tameInput.equals("true")) {
+                    isTame = true;
+                    break;
+                } else if (tameInput.equals("false")) {
+                    isTame = false;
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter 'true' or 'false'.");
+                }
+            }
+
+            Horse horse = new Horse(name, weight, isTame);
+            stable.addHorse(horse);
+        }
+
+        stable.printHorses();
+    }
 }
